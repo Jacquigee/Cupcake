57802ae3f55b2c2865f519f78562a39df12ad979
Jcquigee <gitaujaquiline@gmail.com>
2023-01-24 13:20:18 +0300

    Setting lifecycle owner to observe LiveData

--- app/src/main/java/com/example/cupcake/PickupFragment.kt
+++ app/src/main/java/com/example/cupcake/PickupFragment.kt
@@ -50,6 +50,7 @@ class PickupFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding?.apply {
+            lifecycleOwner = viewLifecycleOwner
             viewModel = sharedViewModel
             nextButton.setOnClickListener { goToNextScreen() }
         }
--- app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
+++ app/src/main/java/com/example/cupcake/model/OrderViewModel.kt
@@ -7,6 +7,7 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 private const val PRICE_PER_CUPCAKE = 2.00
+private const val PRICE_FOR_SAME_DAY_PICKUP = 3.00
 class OrderViewModel: ViewModel() {
 
     private val _quantity = MutableLiveData<Int>()
@@ -38,6 +39,7 @@ class OrderViewModel: ViewModel() {
 
     fun setDate(pickupDate: String){
         _date.value = pickupDate
+        updatePrice()
     }
 
     fun hasNoFlavorSet(): Boolean {
@@ -64,6 +66,10 @@ class OrderViewModel: ViewModel() {
     }
 
     private fun updatePrice(){
-        _price.value = (quantity.value ?: 0) * PRICE_PER_CUPCAKE
+        var calculatedPrice = (quantity.value ?: 0) * PRICE_PER_CUPCAKE
+        if (dateOptions[0] == _date.value){
+            calculatedPrice += PRICE_FOR_SAME_DAY_PICKUP
+        }
+        _price.value = calculatedPrice
     }
 }
